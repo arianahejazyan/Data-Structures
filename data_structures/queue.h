787846196466ee9bcc59fce5bc3a036b3217b226
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-08-21 13:05:23 +0330

    feat: add push and pop for Queue

--- data_structures/queue.h
+++ data_structures/queue.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <utility>
 
 namespace Ariana 
 {
@@ -10,9 +11,48 @@ struct Queue // FIFO
 
         struct Node;
 
-        Node* m_head;
-        Node* m_tail;
+        Node* front;
+        Node* rear;
+
+    public:
+
+        void push(const T& item);
+
+        void push(T&& item);
+
+        void pop();
 };
+
+template<typename T>
+void Queue<T>::push(const T& item) {
+    Node* node = new Node(item);
+    if (rear) {
+        node->next = rear;
+        rear = node;
+    } else {
+        front = rear = node;
+    }node;
+}
+
+template<typename T>
+void Queue<T>::push(T&& item) {
+    Node* node = new Node(std::move(item));
+    if (rear) {
+        node->next = rear;
+        rear = node;
+    } else {
+        front = rear = node;
+    }
+}
+
+template<typename T>
+void Queue<T>::pop() {
+    if (front) {
+        Node* tmp = front;
+        front = front->next;
+        delete tmp;
+    }
+}
 
 template<typename T>
 struct Queue<T>::Node
