b8c054d68a98bd174dcb9a6f89509928919f13eb
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-08-21 13:09:45 +0330

    feat: add front method for Queue

--- data_structures/queue.h
+++ data_structures/queue.h
@@ -11,8 +11,8 @@ struct Queue // FIFO
 
         struct Node;
 
-        Node* front;
-        Node* rear;
+        Node* m_front;
+        Node* m_rear;
 
     public:
 
@@ -21,37 +21,50 @@ struct Queue // FIFO
         void push(T&& item);
 
         void pop();
+
+        T& front();
+
+        const T& front() const;
 };
 
 template<typename T>
 void Queue<T>::push(const T& item) {
     Node* node = new Node(item);
-    if (rear) {
-        node->next = rear;
-        rear = node;
+    if (m_rear) {
+        node->next = m_rear;
+        m_rear = node;
     } else {
-        front = rear = node;
+        m_front = m_rear = node;
     }node;
 }
 
 template<typename T>
 void Queue<T>::push(T&& item) {
     Node* node = new Node(std::move(item));
-    if (rear) {
-        node->next = rear;
-        rear = node;
+    if (m_rear) {
+        node->next = m_rear;
+        m_rear = node;
     } else {
-        front = rear = node;
+        m_front = m_rear = node;
     }
 }
 
 template<typename T>
 void Queue<T>::pop() {
-    if (front) {
-        Node* tmp = front;
-        front = front->next;
+    if (m_front) {
+        Node* tmp = m_front;
+        m_front = m_front->next;
         delete tmp;
     }
+}
+template<typename T>
+T& Queue<T>::front() {
+    return m_front->data;
+}
+
+template<typename T>
+const T& Queue<T>::front() const {
+    return m_front->data;
 }
 
 template<typename T>
