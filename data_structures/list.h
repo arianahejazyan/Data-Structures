e951e6da05b81a2c100a24a1cd2d814e2b4cd656
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-08-21 10:26:47 +0330

    feat: implement constructors

--- data_structures/list.h
+++ data_structures/list.h
@@ -14,6 +14,24 @@ struct List // doubly linked list
         Node* m_head;
         Node* m_tail;
         size_t m_size;
+    
+    public:
+
+        List();
+
+        ~List();
 };
+
+template<typename T>
+List<T>::List(): m_head(nullptr), m_tail(nullptr), m_size(0) {}
+
+template<typename T>
+List<T>::~List() {
+    while (m_head) {
+        Node* tmp = m_head;
+        m_head = m_head->next;
+        delete tmp;
+    }
+}
 
 }; // namespace
