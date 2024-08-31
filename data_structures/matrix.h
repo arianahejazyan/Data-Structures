997e0d1265f2b989eb24ade1fcb5a7324f05dd86
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-08-31 19:55:25 +0330

    feat: add constructors for Matrix

--- data_structures/matrix.h
+++ data_structures/matrix.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 
 namespace Ariana
 {
@@ -6,7 +7,27 @@ namespace Ariana
 template<typename T>
 struct Matrix
 {
+    private:
 
+        T* m_elements;
+        size_t m_rows;
+        size_t m_cols;
+
+    public:
+
+        Matrix(size_t rows, size_t cols);
+
+        ~Matrix();
 };
+
+template<typename T>
+Matrix<T>::Matrix(size_t rows, size_t cols): m_rows(rows), m_cols(cols) {
+    m_elements = new T[m_rows * m_cols];
+}
+
+template<typename T>
+Matrix<T>::~Matrix() {
+    delete[] m_elements;
+}
 
 }; // namespace
