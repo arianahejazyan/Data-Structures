be26645945cb2ccaa053c8f1866225dfe3deecd1
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-09-16 11:21:41 +0330

    refactor: simplify implementation

--- data_structures/array.h
+++ data_structures/array.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cstddef>
-#include <initializer_list>
 
 namespace Ariana
 {
@@ -8,161 +7,94 @@ namespace Ariana
 template<typename T>
 struct Array
 {
-    using reference = T&;
-    using const_reference = const T&;
-
     private:
 
-        T* m_elements;
-        size_t m_capacity;
-        size_t m_size;
+        T* elements;
+        size_t capacity;
+        size_t size;
 
-        class Iterator;
-        class CIterator;
-
-        void allocate();
-
-        void deallocate();
+        void resize();
 
     public:
 
         Array();
 
-        Array(std::initializer_list<T>&& list);
-
-        Array(const Array<T>& other);
-
-        Array(Array<T>&& other) noexcept;
-
         ~Array();
 
-        reference operator[](std::size_t n);
-
-        const_reference operator[](std::size_t n) const;
-
-        Array<T>& operator=(const Array<T>& other);
+        T& operator[](size_t index);
 
         template<typename... Args>
-        void emplace_back(Args&&... args);
+        void emplace(Args&&... args);
 
-        void push_back(const T& element);
+        void push(const T& element);
 
-        void push_back(T&& element);
+        void push(T&& element);
 
-        void pop_back();
+        void pop();
 
-        size_t capacity() const;
+        size_t getCapacity() const;
 
-        size_t size() const;
+        size_t getSize() const;
 
-        Iterator begin();
+        T* begin();
 
-        Iterator end();
+        T* end();
 };
 
 template<typename T>
-Array<T>::Array(): m_size(0), m_capacity(1) {
-    m_elements = new T[m_capacity];
+Array<T>::Array(): size(0), capacity(1) {
+    elements = new T[capacity];
 }
-
-template<typename T>
-Array<T>::Array(std::initializer_list<T>&& list): m_size(list.size()), m_capacity(list.size()) {
-    m_elements = new T[m_capacity];
-    for (int i = 0; i < list.size(); i++) {
-        m_elements[i] = list[i];
-    }
-}
-
-template<typename T>
-Array<T>::Array(const Array<T>& other): m_size(other.size()), m_capacity(other.size()) {
-    m_elements = new T[m_capacity];
-    for (int i = 0; i < other.size(); i++) {
-        m_elements[i] = other[i];
-    }
-}
-
-template<typename T>
-Matrix<T>::Matrix(Matrix<T>&& other) noexcept: rows(other.rows), cols(other.cols), data(other.data) {
-    other.data = nullptr;
-}
-
 
 template<typename T>
 Array<T>::~Array() {
-    delete[] m_elements;
+    delete[] elements;
 }
 
 template<typename T>
 T& Array<T>::operator[](size_t index) {
-    return *(m_elements + index);
+    return *(elements + index);
 }
 
 template<typename T>
-const T& Array<T>::operator[](size_t index) const {
-    return m_elements[index];
-}
-
-template<typename T>
-Array<T>& Array<T>::operator=(const Array<T>& other) {
-    if (this != &other) {
-
-    }
-    return *this;
-}
-
-template<typename T>
-size_t Array<T>::capacity() const {
-    return m_capacity;
-}
-
-template<typename T>
-size_t Array<T>::size() const {
-    return m_size;
-}
-
-template<typename T>
-typename Array<T>::Iterator Array<T>::begin() {
-    return Iterator(m_elements);
-}
-
-template<typename T>
-typename Array<T>::Iterator Array<T>::end() {
-    return Iterator(m_elements + m_size);
-}
-
-template<typename T>
-class Array<T>::Iterator
-{
-    private:
-
-        T* ptr;
+template<typename... Args>
+void Array<T>::emplace(Args&&... args) {
     
-    public:
+}
 
-        Iterator(T* p): ptr(p) {}
+template<typename T>
+void Array<T>::push(const T& element) {
+    
+}
 
-        T& operator*() const {
-            return *ptr;
-        }
+template<typename T>
+void Array<T>::push(T&& element) {
+    
+}
 
-        T* operator->() {
-            return ptr;
-        }
+template<typename T>
+void Array<T>::pop() {
+    --size;
+}
 
-        Iterator& operator++() {
-            ++ptr;
-            return *this;
-        }
+template<typename T>
+size_t Array<T>::getCapacity() const {
+    return capacity;
+}
 
-        Iterator operator++(int) {
-            Iterator tmp = *this;
-            ++(*this);
-            return tmp;
-        }
+template<typename T>
+size_t Array<T>::getSize() const {
+    return size;
+}
 
-        friend bool operator!=(const Iterator& a, const Iterator& b) {
-            return a.ptr != b.ptr;
-        }
-};
+template<typename T>
+T* Array<T>::begin() {
+    return elements;
+}
+
+template<typename T>
+T* Array<T>::end() {
+    return elements + size;
+}
 
 }; // namespace
